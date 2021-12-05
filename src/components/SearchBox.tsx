1a999f3fa786b5ca70187b1f56446441df37a615
Anthony Zamarro <azamarro@veracode.com>
2021-12-05 11:58:52 -0500

    create button for adding Todo on click.

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,28 +1,51 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 interface Props {
     addTodo: (text: string) => void
 }
 
-
 const SearchBox: React.FC<Props> = ({ addTodo }) => {
-    // const [userText, updateUserText] = useState('');
+    const [todoText, updateTodoText] = useState('');
 
     const handleChange = (text: string) => {
-        // console.log('SearchBox', text);
-        addTodo(text);
+        updateTodoText(text);
+    }
+
+    const handleClick = () => {
+        console.log('handleClick');
+        addTodo(todoText);
     }
 
     return (
-        <>
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                m: '2em'
+            }}
+        >
             <TextField 
                 id="standard-basic" 
                 label="Standard" 
                 variant="standard" 
                 onChange={e => handleChange(e.currentTarget.value)}
-                />
-        </>
+                sx={{
+                    mr: '16px'
+                }}
+            />
+            <Button 
+                variant="contained"
+                size="small"
+                sx={{alignSelf: 'flex-end'}}
+                onClick={handleClick}
+            >
+                Add Todo
+            </Button>
+        </Box>
     )
 }
 
